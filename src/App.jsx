1061c480bda64ff8391cbb78833945875dad0768
Lucas Le Gall <Lucas.le_gall@etu.minesparis.psl.eu>
2024-02-16 16:54:07 +0100

    classique

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
         <a href="https://www.youtube.com/watch?v=m7l7455MLzw">Quoicoubeh!</a>
       </p>
       <p className="text-4xl text-green-700 text-center py-20">
-        <a href="https://www.youtube.com/watch?v=crCLXfXtBnk">Quoicoubeh!</a>
+        <a href="https://www.youtube.com/watch?v=crCLXfXtBnk">Apagnan!</a>
       </p>
     </div>
     );
