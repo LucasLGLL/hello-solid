cfffe12174a332bcb2d2fd377c82aeeb6db845a3
Lucas Le Gall <Lucas.le_gall@etu.minesparis.psl.eu>
2024-02-16 16:51:42 +0100

    classique akhy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Quoicoubeh!</p>
-  );
+    <div>
+    <p className="text-4xl text-green-700 text-center py-20">
+        <a href="https://www.youtube.com/watch?v=m7l7455MLzw">Quoicoubeh!</a>
+      </p>
+      <p className="text-4xl text-green-700 text-center py-20">
+        <a href="https://www.youtube.com/watch?v=crCLXfXtBnk">Quoicoubeh!</a>
+      </p>
+    </div>
+    );
 }
 
 export default App;
